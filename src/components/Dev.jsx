7062feb572019a207cbcf8cdac82bd67396734e6
Mikun Flo <folarincelesteenah@gmail.com>
2026-03-03 17:08:42 +0100

    Feat: Add Dev dataset

--- src/components/Dev.jsx
+++ src/components/Dev.jsx
@@ -9,4 +9,48 @@ function Dev(props) {
   );
 }
 
-export default Dev;
+//export default Dev;
+
+import { useState } from "react";
+
+export default function AddDev() {
+  const [dev, setDev] = useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+    courses: [],
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDev((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    console.log("Collected dev data:", dev);
+  };
+
+  return (
+    <div>
+      <input
+        name="firstName"
+        value={dev.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      <input
+        name="lastName"
+        value={dev.lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      <input
+        name="age"
+        value={dev.age}
+        onChange={handleChange}
+        placeholder="Age"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
