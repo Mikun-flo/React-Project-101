3b9540714f1fc042917b91bb56f545da91750ee5
Mikun Flo <folarincelesteenah@gmail.com>
2026-03-10 18:55:00 +0100

    Feat: Refactor Card layout and styles for improved responsiveness and design

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,40 @@ export default function App() {
     <div>
       <Header></Header>
       <Projects></Projects>
-      <Card
-        title="Project 1 - Web App Dashboard"
-        text="Interactive analytics dashboard with real-time charts."
-      />
-      <Card
-        title="Project 2 - The 24th Avenue"
-        text="Full-stack online store with cart & payments."
-      />
-      <Card
-        title="Project 3 - Birthday Countdown"
-        text="A web timer that counts down to a particular set date."
-      />
-      <Card
-        title="Project 4 - Jewelry Blog"
-        text="A short article blog about jewelry trends."
-      />
-      <Card
-        title="Project 5 - Suite V2 - Landing Page"
-        text="A mock up web landing page."
-      />
-      <Card
-        title="Project 6 - The 24th Avenue E-commerce"
-        text="A Jewelry E-commerce website."
-      />
-      <Card
-        title="Project 7 - Ayomikun's Portfolio"
-        text="Personal portfolio website."
-      />
-      <Card
-        title="Project 8 - Placeholder Text"
-        text="This is just a placeholder text."
-      />
+      <div className="cards-container">
+        <Card
+          title="Project 1 - Web App Dashboard"
+          text="Interactive analytics dashboard with real-time charts."
+        />
+        <Card
+          title="Project 2 - The 24th Avenue"
+          text="Full-stack online store with cart & payments."
+        />
+        <Card
+          title="Project 3 - Birthday Countdown"
+          text="A web timer that counts down to a particular set date."
+        />
+        <Card
+          title="Project 4 - Jewelry Blog"
+          text="A short article blog about jewelry trends."
+        />
+        <Card
+          title="Project 5 - Suite V2 - Landing Page"
+          text="A mock up web landing page."
+        />
+        <Card
+          title="Project 6 - The 24th Avenue E-commerce"
+          text="A Jewelry E-commerce website."
+        />
+        <Card
+          title="Project 7 - Ayomikun's Portfolio"
+          text="Personal portfolio website."
+        />
+        <Card
+          title="Project 8 - Placeholder Text"
+          text="This is just a placeholder text."
+        />
+      </div>
       <hr></hr>
 
       <Button></Button>
